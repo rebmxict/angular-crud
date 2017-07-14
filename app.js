4c191565c947019c8c2e01b2ae866606a7d3244c
meradossi <miraleon405@yandex.com>
2017-07-14 20:23:38 +0800

    five field added

--- app.js
+++ app.js
@@ -2,66 +2,113 @@ function TodosController($scope) {
     $scope.estimates = [0, 1, 2, 3, 5, 8];
 
     $scope.todos = [{
-        name: "Learn angular",
-        estimate: 8,
-        done: true},
-    {
-        name: "Install java",
-        estimate: 2,
-        done: false},
-    {
-        name: 'Uninstall ruby',
-        estimate: 3,
-        done: false}];
-        
+            id: "Learn angular",
+            name: "sample name",
+            destination: "sample destination",
+            creation_date: "sample date",
+            client_id: "sample client id",
+            estimate: 8,
+            done: true
+        },
+        {
+            id: "Learn angular",
+            name: "sample name",
+            destination: "sample destination",
+            creation_date: "sample date",
+            client_id: "sample client id",
+            estimate: 2,
+            done: false
+        },
+        {
+            name: 'Uninstall ruby',
+            estimate: 3,
+            done: false
+        }
+    ];
+
     $scope.addTodo = function() {
-        if ($scope.todoName === "") {
+        if ($scope.id === "") {
+            return false;
+        }
+        if ($scope.name === "") {
+            return false;
+        }
+        if ($scope.destination === "") {
+            return false;
+        }
+        if ($scope.creation_date === "") {
+            return false;
+        }
+        if ($scope.client_id === "") {
             return false;
         }
 
         $scope.todos.push({
-            name: $scope.todoName,
+            id: $scope.id,
+            name: $scope.name,
+            destination: $scope.destination,
+            creation_date: $scope.creation_date,
+            client_id: $scope.client_id,
             estimate: $scope.todoEstimate,
             done: false
         });
 
-        $scope.todoName = '';
+        $scope.id = '';
         $scope.todoEstimate = 0;
     }
-        
+
     $scope.sum = function(list, done) {
-       var total = 0;
-       angular.forEach(list, function(item) {
-           if (item.done == done) total += item.estimate;
-       });
-       return total;    
+        var total = 0;
+        angular.forEach(list, function(item) {
+            if (item.done == done) total += item.estimate;
+        });
+        return total;
     }
-    
+
 }
 
 function TodoEditorController($scope) {
     $scope.editorEnabled = false;
 
     $scope.enableEditor = function() {
-        $scope.editorEnabled = true;
+            $scope.editorEnabled = true;
 
-        $scope.todoName = $scope.todo.name;
-        $scope.todoEstimate = $scope.todo.estimate;
-    },
+            $scope.id = $scope.todo.id;
+            $scope.name = $scope.todo.name;
+            $scope.destination = $scope.todo.destination;
+            $scope.creation_date = $scope.todo.creation_date;
+            $scope.client_id = $scope.todo.client_id;
+            $scope.todoEstimate = $scope.todo.estimate;
+        },
 
-    $scope.disableEditor = function() {
-        $scope.editorEnabled = false;
-    },
+        $scope.disableEditor = function() {
+            $scope.editorEnabled = false;
+        },
 
-    $scope.save = function() {
-        if ($scope.todoName === "") {
-            return false;
+        $scope.save = function() {
+            if ($scope.id === "") {
+                return false;
+            }
+            if ($scope.name === "") {
+                return false;
+            }
+            if ($scope.destination === "") {
+                return false;
+            }
+            if ($scope.creation_date === "") {
+                return false;
+            }
+            if ($scope.client_id === "") {
+                return false;
+            }
+
+            $scope.todo.id = $scope.id;
+            $scope.todo.name = $scope.name;
+            $scope.todo.destination = $scope.destination;
+            $scope.todo.creation_date = $scope.creation_date;
+            $scope.todo.client_id = $scope.client_id;
+            $scope.todo.estimate = $scope.todoEstimate;
+
+            $scope.disableEditor();
         }
-
-        $scope.todo.name = $scope.todoName;
-        $scope.todo.estimate = $scope.todoEstimate;
-
-        $scope.disableEditor();
-    }
 }
-
